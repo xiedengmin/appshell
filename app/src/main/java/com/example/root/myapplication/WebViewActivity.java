90f43ab286b78593ed410e0fb48c5463b9581881
xiedengmin <7274796@qq.com>
2019-03-28 20:23:36 +0800

    test

---
+++ app/src/main/java/com/example/root/myapplication/WebViewActivity.java
@@ -0,0 +1,81 @@
+package com.example.root.myapplication;
+
+/**
+ * Created by root on 18-3-26.
+ */
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebViewActivity  extends AppCompatActivity {
+    private WebView webView;
+ //   private String url = "http://dai.sqove.cn";
+  //  private String url = "http://dai.dhksup.cn";
+ private String url = "http://netjet.club";
+    private String Tag = "MQL";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.webview);
+
+        initView();
+    }
+
+    private void initView(){
+        webView = (WebView)findViewById(R.id.webView);
+
+        //支持javascript
+        webView.getSettings().setJavaScriptEnabled(true);
+        // 设置可以支持缩放
+        webView.getSettings().setSupportZoom(true);
+        // 设置出现缩放工具
+        webView.getSettings().setBuiltInZoomControls(true);
+        //扩大比例的缩放
+        webView.getSettings().setUseWideViewPort(true);
+        //自适应屏幕
+        webView.getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.SINGLE_COLUMN);
+        webView.getSettings().setLoadWithOverviewMode(true);
+
+
+        //如果不设置WebViewClient，请求会跳转系统浏览器
+        webView.setWebViewClient(new WebViewClient() {
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //该方法在Build.VERSION_CODES.LOLLIPOP以前有效，从Build.VERSION_CODES.LOLLIPOP起，建议使用shouldOverrideUrlLoading(WebView, WebResourceRequest)} instead
+                //返回false，意味着请求过程里，不管有多少次的跳转请求（即新的请求地址），均交给webView自己处理，这也是此方法的默认处理
+                //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://ask.csdn.net/questions/178242
+
+                if (url.toString().contains("sina.cn")){
+                    view.loadUrl("http://ask.csdn.net/questions/178242");
+                    return true;
+                }
+
+                return false;
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request)
+            {
+                //返回false，意味着请求过程里，不管有多少次的跳转请求（即新的请求地址），均交给webView自己处理，这也是此方法的默认处理
+                //返回true，说明你自己想根据url，做新的跳转，比如在判断url符合条件的情况下，我想让webView加载http://ask.csdn.net/questions/178242
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    if (request.getUrl().toString().contains("sina.cn")){
+                        view.loadUrl("http://ask.csdn.net/questions/178242");
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+
+        });
+        webView.loadUrl(url);
+    }
+
+}
